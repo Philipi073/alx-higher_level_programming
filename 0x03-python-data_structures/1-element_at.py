23626430c86a0e9e58709e08f2fbe3d2bb58a8af
Okoye Philip <okoyephlp07322@gmail.com>
2022-12-07 03:11:19 -0800

    updated

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,6 @@
 
 def element_at(my_list, idx):
     """
-    retrieves an element from a list like in C
+    retrieves an element from a list like in C.
     """
-    return (my_list[idx] if 0 <= idx <= len(my_list) else None)
-
+    return (my_list[idx] if 0 <= idx < len(my_list) else None)
